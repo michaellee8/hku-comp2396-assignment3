94577255758fc322308bdd68a7b2dd4a27b34ac4
michaellee8 <ckmichael8@gmail.com>
2019-10-28 20:47:34 +0800

    feat: implement CoinPack

--- src/com/example/hku/comp2396/assignment3/CoinPack.java
+++ src/com/example/hku/comp2396/assignment3/CoinPack.java
@@ -1,13 +1,22 @@
 package com.example.hku.comp2396.assignment3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class CoinPack implements Cloneable {
 
   private HashMap<Integer, Integer> coins;
 
+  public CoinPack() {
+    coins = new HashMap<Integer, Integer>();
+  }
+
   // clone() provides a copy of the CoinPack that can be safely modified
-  public Object clone() {
+  public CoinPack clone() {
     CoinPack pack = (CoinPack) super.clone();
     pack.coins = (HashMap<Integer, Integer>) coins.clone();
     return pack;
@@ -18,6 +27,107 @@ public class CoinPack implements Cloneable {
         integerIntegerEntry -> integerIntegerEntry.getValue() * integerIntegerEntry.getKey()).sum();
   }
 
-  public Integer
+  public Integer getCoinTotalCount() {
+    return coins.entrySet().stream().mapToInt(Entry::getValue)
+        .sum();
+  }
+
+  public Integer getCoinCountOfType(Integer i) {
+    return coins.getOrDefault(i, 0);
+  }
+
+  public boolean hasCoinType(Integer i) {
+    return coins.containsKey(i);
+  }
+
+  public boolean addCoin(Integer i) {
+    coins.put(i, coins.getOrDefault(i, 0) + 1);
+    return true;
+  }
+
+  public boolean removeCoin(Integer i) {
+    assert coins.getOrDefault(i, 1) > 0 : "coin value must not be less than 1 !";
+    if (!coins.containsKey(i)) {
+      return false;
+    } else if (coins.get(i) > 1) {
+      coins.put(i, coins.get(i) - 1);
+    } else if (coins.get(i) == 1) {
+      coins.remove(i);
+    }
+    return true;
+  }
+
+  public void addAll(CoinPack cp) {
+    for (var entry : cp.coins.entrySet()) {
+      coins.put(entry.getKey(), coins.getOrDefault(entry.getKey(), 0) + entry.getValue());
+    }
+  }
+
+  public CoinPack cloneAndAddCoin(Integer i) throws InvalidOperationException {
+    var pack = this.clone();
+    if (!pack.addCoin(i)) {
+      throw new InvalidOperationException();
+    }
+    return pack;
+  }
+
+  public CoinPack cloneAndRemoveCoin(Integer i) throws InvalidOperationException {
+    var pack = this.clone();
+    if (!pack.removeCoin(i)) {
+      throw new InvalidOperationException();
+    }
+    return pack;
+  }
+
+  public CoinPack cloneAndAddAll(CoinPack cp) {
+    var pack = this.clone();
+    pack.addAll(cp);
+    return pack;
+  }
+
+  public List<Integer> getAvailableCoinTypes() {
+    var l = new ArrayList<Integer>(coins.keySet());
+    l.sort(Comparator.reverseOrder());
+    return l;
+  }
+
+  public boolean isEmpty() {
+    return coins.isEmpty();
+  }
+
+  public static CoinPack attemptAmount(CoinPack cp, Integer amt) throws InvalidOperationException {
+
+    // No more coins
+    if (cp.isEmpty()) {
+      return null;
+    }
+
+    // The value of the least valued coin is already larger than amt
+    if (cp.getAvailableCoinTypes().get(cp.getAvailableCoinTypes().size() - 1) > amt) {
+      return null;
+    }
+
+    // Return the pack containing such coin if current CoinPack contains the exact coin of value amt
+    for (var i : cp.getAvailableCoinTypes()) {
+      if (i.equals(amt)) {
+        return new CoinPack().cloneAndAddCoin(i);
+      }
+    }
+
+    // Attempt to transverse down the tree by subtracting one coin from both the amount and the pack
+    for (var i : cp.getAvailableCoinTypes().stream().filter(integer -> integer < amt).collect(
+        Collectors.toList())) {
+      var attempt = attemptAmount(cp.cloneAndRemoveCoin(i), amt - i);
+      if (attempt != null) {
+        assert attempt.getSum() == amt - i;
+        // If such pack with sum as the required amount is found, add the current value to it and then pass it up
+        return attempt.cloneAndAddCoin(i);
+      }
+    }
+
+    return null;
+
+  }
 
 }
+
---
+++ src/com/example/hku/comp2396/assignment3/InvalidOperationException.java
@@ -0,0 +1,5 @@
+package com.example.hku.comp2396.assignment3;
+
+public class InvalidOperationException extends Exception {
+
+}
